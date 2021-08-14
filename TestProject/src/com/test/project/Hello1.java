12ad668aeeb897df540ca9a22ca9e6166068f497
dipab <dipab@DESKTOP-ESR0C30>
2021-08-14 18:35:23 +0530

    ADM

--- TestProject/src/com/test/project/Hello1.java
+++ TestProject/src/com/test/project/Hello1.java
@@ -3,7 +3,8 @@ package com.test.project;
 public class Hello1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Hello_test2");
+    System.out.println("Hiello_test2");
+    System.out.println("Hello_testADM");
 	}
 
 }
